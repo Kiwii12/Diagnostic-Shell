ced3fc4b8c191520bc73a2d53401f7b37acc3de6
Johnathan Ackerman <johnny.ackerman@bowtaps.com>
2016-02-08 15:15:02 -0700

    Will tokenize string
    - will not pull first value from it

--- prog1.cpp
+++ prog1.cpp
@@ -1,19 +1,47 @@
 #include <iostream>
 #include <string>
 
+//For Parsing the bloody entry because C++ doesn't seem to have a built in 
+//function for such a task
+#include <vector>
+#include <sstream>
+
 using namespace std;
 
 int main()
 {
+	//For parsing the shell arguments
+	vector<string> entry;
+
     string entry1;
     string entry2;
+
+
+	// Faking being a shell here
     cout << "dsh >";
-    while(getline(cin,entry1))
+
+    while(true)
     {
-        if( entry1 == "\n")
-        {
-            break;  
-        }
+		// Grab cin inside the infinant loop in the off chance the user wants
+		// to maybe use a second command
+		getline(cin, entry1);
+		istringstream myStringStream(entry1);
+		while (getline(myStringStream, entry2, ' '))
+		{
+			entry.push_back(entry1);
+		}
+		if (!entry.empty())
+		{
+			entry1 = entry[0];
+		}
+
+		// Start of "Switch"
+		if (entry.size() > 2)
+		{
+			cout << " Too many commands " << endl;
+		}
+        else if( entry1 == "\n")
+        { }
         else if( entry1 == "cmdnm" )
         {
             
